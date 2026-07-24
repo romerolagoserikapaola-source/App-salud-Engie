6012364eaa0545dcf4ab86d6ac857bab1b082f46
SSOMA_Engie <romerolagoserikapaola@gmail.com>
2026-07-23 20:39:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// Reemplaza esta URL luego de desplegar Apps Script como "Aplicación web".
 window.APP_CONFIG = {
-  API_URL: "PEGAR_AQUI_URL_DE_APPS_SCRIPT",
+  API_URL: "https://script.google.com/macros/s/AKfycbz0WahSh5tlFpU6Dz5aeZvVb98gA1METaxIrzytaaYxlWZIA5ug9pHy1dTtY3kxnUN4Pg/exec",
   APP_NAME: "Control Preventivo de Salud ENGIE"
 };
