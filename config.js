b4d767c01c98a9a78e3bdcaeb6ec7a3f987a96a2
SSOMA_Engie <romerolagoserikapaola@gmail.com>
2026-07-26 22:01:33 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz0WahSh5tlFpU6Dz5aeZvVb98gA1METaxIrzytaaYxlWZIA5ug9pHy1dTtY3kxnUN4Pg/exec",
+  API_URL:https://script.google.com/macros/s/AKfycbz0WahSh5tlFpU6Dz5aeZvVb98gA1METaxIrzytaaYxlWZIA5ug9pHy1dTtY3kxnUN4Pg/exec,
   APP_NAME: "Control Preventivo de Salud ENGIE",
   ADMIN_PIN: "2026ENGIE"
 };
